9c58dc14fc87676475e100d0f62e6734ba075b3a
ggila <ggilaber@student.42.fr>
2016-06-03 09:19:09 +0200

    bfs works, to be cleaned

--- src/compute/compute_way.c
+++ src/compute/compute_way.c
@@ -6,7 +6,7 @@
 /*   By: ggilaber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/30 15:46:33 by ggilaber          #+#    #+#             */
-/*   Updated: 2016/06/02 20:58:32 by ggilaber         ###   ########.fr       */
+/*   Updated: 2016/06/03 09:01:20 by ggilaber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,17 @@ static void			bfs(t_queue *upcoming, t_set *visited, t_anthill *anthill)
 {
 	t_qval	*node;
 
+	ft_printf("\n\nvisited: ");
+	set_print(visited, str_print);
+	ft_printf("\n");
 	if ((node = queue_pop(upcoming)) == NULL)
 		return ;
-//	ft_printf("%s\n", node->node_id);
 	if (ft_strequ(node->node_id, anthill->end))
+	{
+		ft_printf("\nway: ", node->node_id);
 		set_print(node->way, str_print);
 //		add_way(anthill, node->way);
+	}
 	else
 		visit(node, upcoming, visited, &anthill->graph);
 	free(node);
@@ -74,5 +79,6 @@ void				compute_way(t_anthill *anthill, char *start)
 
 	init_bfs_struct(anthill, start, &visited, &upcoming);
 	bfs(&upcoming, &visited, anthill);
+	ft_printf("\n");
 	set_free(&visited);
 }
--- src/compute/visit.c
+++ src/compute/visit.c
@@ -6,7 +6,7 @@
 /*   By: ggilaber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/01 10:15:32 by ggilaber          #+#    #+#             */
-/*   Updated: 2016/06/02 21:07:21 by ggilaber         ###   ########.fr       */
+/*   Updated: 2016/06/03 09:11:14 by ggilaber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ static void	add_id(char *id, t_set *way, t_queue *queue)
 	t_qval	*new_q_node;
 
 	new_way = queue->tail ? way : set_copy(way);
-//	ft_printf("%p\n", new_way);
 	new_q_node = new_queue_node(id, new_way);
 	queue_push(queue, new_q_node);
 }
@@ -32,21 +31,26 @@ static void	add_id(char *id, t_set *way, t_queue *queue)
 void	visit(t_qval *q_node, t_queue *upcoming,
 					t_set *visited, t_graph *graph)
 {
-	t_set	*neighbours;
-	t_queue	new_upcoming;
-	char	*id;
-	bool	flag;
+	t_set		*neighbours;
+	t_set_it	it;
+	t_queue		new_upcoming;
+	char		*id;
+	bool		flag;
 
 	flag = false;
 	queue_init(&new_upcoming);
+	set_it_init(&it);
 	neighbours = GRAPH_GET_NEIGHBOUR(graph, q_node->node_id);
-	set_print(neighbours, str_print);
-	while ((id = set_getnextelem(neighbours)))
+//	set_print(neighbours, str_print);
+	ft_printf("\nvisit: %s\t", q_node->node_id);
+	while ((id = set_getnextelem(neighbours, &it)))
 	{
-		ft_printf("\n%s\t", id);
-		set_print(visited, str_print);
+//		ft_printf("\n%s\t", id);
 		if (!set_isin(visited, id))
 		{
+			ft_printf("%s ", id);
+			if (!ft_strequ(id, "g"))
+			set_put(visited, id);
 			add_id(id, q_node->way, &new_upcoming);
 			flag = true;
 		}
